da7d2d6bfb0f5587aca8ba1b37806db0de62a5ee
Ariel <aritem@gmail.com>
2017-08-08 00:53:36 -0400

    final caching fix

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from ediblepickle import checkpoint
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.vars = {}
 
 @app.route('/')
