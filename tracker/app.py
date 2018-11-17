3057d0bc5bcd83c4f039e87adb2d250e91777c75
Ariel M'ndange-Pfupfu <ariel.mp@capitalone.com>
2018-11-17 14:13:56 -0500

    allow send_file caching

--- tracker/app.py
+++ tracker/app.py
@@ -9,7 +9,6 @@ from flask import Flask, render_template, request, redirect, url_for, send_file
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # prevents browser caching maps
 app.vars = {}
 
 @app.route('/')
